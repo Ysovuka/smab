b26277fab7b63f70af9426d0b201f6e1ba7581d8
Joshua Thompson <ysovuka@gmail.com>
2017-06-10 14:45:17 -0500

    Updated Dice Implementation.
    Added new command for dice, allowing to roll # amount of # sided dice adding, subtracting, multiplying or dividing the results by #.

--- src/Gaming/Smab.Systems.Gaming/Dice.cs
+++ src/Gaming/Smab.Systems.Gaming/Dice.cs
@@ -8,17 +8,61 @@ namespace Smab.Systems.Gaming
         private RandomNumberGenerator rnd = RandomNumberGenerator.Create();
         private byte[] mbuffer = new byte[8];
 
+        public double Roll(double times, double size, string @operator, double number)
+        {
+            if (!Validate(times, size, @operator, number)) throw new InvalidOperationException();
+
+            double total = 0;
+            for (int i = 1; i <= times; i++)
+            {
+                total += Solve(Roll(size), number, @operator);
+            }
+            
+            if (total < 1) return 1;
+
+            return total;
+        }
+
         public double Roll(double max)
         {
             rnd.GetBytes(mbuffer);
             UInt64 rand = BitConverter.ToUInt64(mbuffer, 0);
             double results = rand / (1.0 + UInt64.MaxValue);
-            return results * max;
+            return Math.Ceiling(results * max);
         }
 
         public int Roll(int max)
         {
             return (int)Roll((double)max);
         }
+
+        private bool Validate(double times, double size, string @operator, double number)
+        {
+            if (times == 0) return false;
+            if (size == 0) return false;
+            if (@operator.Equals("*") && number == 0) return false;
+            if (@operator.Equals("/") && number == 0) return false;
+            if (Math.Pow(times, size) > double.MaxValue) return false;
+
+            return true;
+        }
+
+        private double Solve(double left, double right, string @operator = "")
+        {
+            switch (@operator)
+            {
+                case "*":
+                    return left * right;
+                case "/":
+                    return left / right;
+                case "-":
+                    return left - right;
+                case "+":
+                default:
+                    return left + right;
+            }
+        }
+
+
     }
 }
--- src/Platforms/Discord/Smab.Platforms.Discord.Bot/Commands/Gaming/DiceCommand.cs
+++ src/Platforms/Discord/Smab.Platforms.Discord.Bot/Commands/Gaming/DiceCommand.cs
@@ -1,6 +1,7 @@
 ﻿using Discord.Commands;
 using Smab.Systems.Gaming;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,8 +18,34 @@ namespace Smab.Platforms.Discord.Commands.Gaming
                 Dice die = new Dice();
                 await Context.Channel.SendMessageAsync($"{Context.Message.Author.Username} rolls {Math.Round(die.Roll(maximum))}.");
             }
-            catch (Exception ex)
+            catch
             {
+                await Context.Channel.SendMessageAsync("Unable to get dice from the void.");
+            }
+        }
+
+        [Command("dice roll"), Summary("Rolls a die between 1 and 100 or the supplied maximum number.")]
+        public async Task ExecutePlus(string diceType = "1d100", string @operator = "", double number = 0)
+        {
+            try
+            {
+                string[] diceTypeSplit = diceType.ToLower().Split('d');
+                if (diceTypeSplit.Length <= 0 || diceTypeSplit.Length >= 3)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                long diceAmount = Convert.ToInt64(diceTypeSplit[0].Replace("d", ""));
+                long diceSize = Convert.ToInt64(diceTypeSplit[1].Replace("d", ""));
+
+                Dice die = new Dice();
+
+                await Context.Channel.SendMessageAsync
+                    ($"{Context.Message.Author.Username} rolls {die.Roll(diceAmount, diceSize, @operator, number)}.");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
                 await Context.Channel.SendMessageAsync("Unable to get dice from the void.");
             }
         }
