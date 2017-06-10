2bae0f031ca906cbda80fce6cfcf34ec328d9477
Joshua Thompson <ysovuka@gmail.com>
2017-06-09 19:22:08 -0500

    Mute/Unmute Command 90% Implemented.
    Need to add duration mechanics to muted feature for users to be automatically unmuted.

---
+++ src/Platforms/Discord/Smab.Platforms.Discord.Bot/Commands/Common/MuteCommand.cs
@@ -0,0 +1,37 @@
+﻿using Discord.Net.WebSockets;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+
+namespace Smab.Platforms.Discord.Commands.Common
+{
+    public class MuteCommand : ModuleBase
+    {
+        [Command("mute"), Summary("Mutes the specified person for the specified duration.")]
+        public async Task Execute(IUser user, string duration)
+        {
+            try
+            {
+                if (!Context.Guild.Roles.Any(r => r.Name == "Muted"))
+                {
+                    await Context.Guild.CreateRoleAsync("Muted", new GuildPermissions());
+                }
+
+                var mutedRole = Context.Guild.Roles.FirstOrDefault(r => r.Name == "Muted");
+                IGuildUser userInfo = user as IGuildUser;
+                await userInfo.AddRoleAsync(mutedRole);
+
+                await Context.Channel.SendMessageAsync($"{userInfo.Username} has been muted.");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                if (user != null) await base.ReplyAsync($"Unable to mute {user.Username}.");
+            }
+        }
+    }
+}
---
+++ src/Platforms/Discord/Smab.Platforms.Discord.Bot/Commands/Common/UnmuteCommand.cs
@@ -0,0 +1,36 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smab.Platforms.Discord.Commands.Common
+{
+    public class UnmuteCommand : ModuleBase
+    {
+        [Command("unmute"), Summary("Mutes the specified person for the specified duration.")]
+        public async Task Execute(IUser user)
+        {
+            try
+            {
+                if (!Context.Guild.Roles.Any(r => r.Name == "Muted"))
+                {
+                    throw new InvalidOperationException();
+                }
+
+                var mutedRole = Context.Guild.Roles.FirstOrDefault(r => r.Name == "Muted");
+                IGuildUser userInfo = user as IGuildUser;
+                await userInfo.RemoveRoleAsync(mutedRole);
+
+                await Context.Channel.SendMessageAsync($"{userInfo.Username} has been unmuted.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                if (user != null) await base.ReplyAsync($"Unable to mute {user.Username}.");
+            }
+        }
+    }
+}
