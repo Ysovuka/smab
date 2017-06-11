f20befab5e79b1bbf0998017bb0e684ad20b6b5f
Joshua Thompson <ysovuka@gmail.com>
2017-06-10 19:17:09 -0500

    Relocated the Gaming project to the proper location.

--- src/Platforms/Discord/Smab.Platforms.Discord.Bot/Commands/Gaming/DiceCommand.cs
+++ src/Platforms/Discord/Smab.Platforms.Discord.Bot/Commands/Gaming/DiceCommand.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Smab.Platforms.Discord.Commands.Gaming
@@ -25,23 +26,40 @@ namespace Smab.Platforms.Discord.Commands.Gaming
         }
 
         [Command("dice roll"), Summary("Rolls a die between 1 and 100 or the supplied maximum number.")]
-        public async Task ExecutePlus(string diceType = "1d100", string @operator = "", double number = 0)
+        public async Task ExecutePlus(string diceType = "1d100", string @operator = "", string inputNumber = "0")
         {
             try
             {
-                string[] diceTypeSplit = diceType.ToLower().Split('d');
-                if (diceTypeSplit.Length <= 0 || diceTypeSplit.Length >= 3)
+                long diceAmount = 0;
+                long diceSize = 0;
+                if (diceType.StartsWith("d"))
                 {
-                    throw new InvalidOperationException();
+                    diceAmount = 1;
+                    diceSize = Convert.ToInt64(diceType.Replace("d", ""));
                 }
+                else
+                {
+                    string[] diceTypeSplit = diceType.ToLower().Split('d');
+                    if (diceTypeSplit.Length <= 0 || diceTypeSplit.Length >= 3)
+                    {
+                        throw new InvalidOperationException();
+                    }
 
-                long diceAmount = Convert.ToInt64(diceTypeSplit[0].Replace("d", ""));
-                long diceSize = Convert.ToInt64(diceTypeSplit[1].Replace("d", ""));
+                    diceAmount = Convert.ToInt64(diceTypeSplit[0].Replace("d", ""));
+                    diceSize = Convert.ToInt64(diceTypeSplit[1].Replace("d", ""));
+                }
+                double number = 0;
+                if (Regex.Matches(inputNumber, @"[a-zA-Z]").Count > 0)
+                    if (!inputNumber.ToLower().Equals("pi"))
+                        throw new InvalidOperationException();
+                    else
+                        number = Math.PI;
+                else
+                    number = Convert.ToDouble(inputNumber);
 
                 Dice die = new Dice();
-
                 await Context.Channel.SendMessageAsync
-                    ($"{Context.Message.Author.Username} rolls {die.Roll(diceAmount, diceSize, @operator, number)}.");
+                    ($"{Context.Message.Author.Username} rolls {Math.Ceiling(die.Roll(diceAmount, diceSize, @operator, number))}.");
             }
             catch(Exception e)
             {
